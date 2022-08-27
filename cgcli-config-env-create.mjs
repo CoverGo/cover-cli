b9e84315ce3f03a7f7c7d51bd3feb6bba13f7395
Richard Vanbergen <rich.vanbergen@gmail.com>
2022-08-27 16:15:25 +0900

    better env config discovery

--- cgcli-config-env-create.mjs
+++ cgcli-config-env-create.mjs
@@ -3,30 +3,36 @@ import { Command } from 'commander'
 import { chalk, question } from 'zx'
 import { exit } from 'node:process'
 import { z } from 'zod'
-import { parse, stringify } from 'yaml'
-import * as fs from 'node:fs/promises';
+import { stringify } from 'yaml'
 import { displayValidationErrors } from './src/validation/error.mjs'
-import { DirectoryNotAccessibleError, getConfigForEnv } from './tenant/file.mjs'
+import { getConfig, writeConfig } from './src/config/config.mjs'
 
 const program = new Command()
 
 program
 	.command('create', 'Configure a new environment')
 	.argument('<alias>', 'The alias you want to use when referencing this environment in other commands')
-	.option('-e, --endpoint <endpoint>', 'The endpoint to use for the graphql API')
+	.option('-d, --description <description>', 'Environment description')
+	.option('-e, --endpoint <endpoint>', 'Graphql endpoint for this environment')
 	.action(async (alias, options) => {
 		const endpoint = options.endpoint
 			? options.endpoint
 			: await question(chalk.bold.blue(`What's the endpoint for this environment? `))
 
+		const description = options.description
+			? options.description
+			: await question(chalk.bold.blue(`(optional) Enter a short description for this environment: `))
+
 		const validation = z.object({
 			alias: z.string().min(1).regex(/[a-zA-Z0-9\-:_]/, { message: "Alias can only contain alphanumeric, -, : and _" }),
-			endpoint: z.string().url()
+			endpoint: z.string().url(),
+			description: z.string()
 		})
 
 		const inputs = {
 			alias,
-			endpoint
+			endpoint,
+			description: description ?? ''
 		}
 
 		try {
@@ -36,35 +42,16 @@ program
 			exit(1)
 		}
 
-		try {
-			const path = await getConfigForEnv()
+		const config = getConfig()
+		const environments = config?.environments ?? {}
+		environments[alias] = { endpoint, description: description ?? "" }
+		config.environments = environments
+		writeConfig(config)
 
-			try {
-				await fs.access(path)
-			} catch {
-				await fs.writeFile(path, '')
-			}
-
-			const contents = await fs.readFile(path, 'utf8')
-			const config = parse(contents) ?? {}
-			const environments = config?.environments ?? {}
-			environments[alias] = { endpoint }
-			config.environments = environments
-			const content = stringify(config)
-			await fs.writeFile(path, content)
-
-			console.log('')
-			console.log(chalk.green.bold(`New environment \`${alias}\` created!`))
-			console.log(chalk.green(content))
-			exit(0)
-		} catch (e) {
-			if (e instanceof DirectoryNotAccessibleError) {
-				console.error(chalk.bgRed(e.message))
-				exit(1)
-			}
-
-			throw e
-		}
+		console.log('')
+		console.log(chalk.green.bold(`New environment \`${alias}\` created!`))
+		console.log(chalk.green(stringify(config)))
+		exit(0)
 	})
 
 program.parse()
--- cgcli-config-env.mjs
+++ cgcli-config-env.mjs
@@ -5,5 +5,7 @@ const program = new Command()
 
 program
 	.command('create', 'Configure a new environment')
+	.command('info', 'Show details of a specific environment')
+	.command('list', 'Show a list of all environment aliases')
 
 program.parse()
