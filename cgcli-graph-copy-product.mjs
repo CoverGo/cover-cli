c6a888ef8c3ccd08893281bd10861bc066f2d6a7
Richard Vanbergen <rich.vanbergen@gmail.com>
2022-08-28 22:47:28 +0900

    completing import

--- cgcli-graph-copy-product.mjs
+++ cgcli-graph-copy-product.mjs
@@ -1,13 +1,138 @@
 #!/usr/bin/env zx
 import { Command } from 'commander'
 import { getTenantWithEnvironment } from './src/config/config.mjs'
-import { useGraphProduct } from './src/graph/product.mjs'
+import {QueryError, useGraphProduct} from './src/graph/product.mjs'
 import { exit } from 'node:process'
 import { fetchNewToken } from "./src/login/login.mjs"
 import { chalk } from 'zx'
 import cliProgress from 'cli-progress'
+import {stringify} from "yaml";
 
 const program = new Command()
+
+function generateId() {
+	return [...Array(8)].map(() => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)).join("")
+}
+
+async function fetchSourceProduct(productId, sourceContext, sourceAlias) {
+	const [plan, name, version] = productId.split('/')
+	console.log(chalk.blue(`${chalk.bold(`1/8:`)} Fetch \`${productId}\` from tenant \`${sourceAlias}\`.`))
+	const product = await sourceContext.fetchProduct(plan, name, version)
+	if (!product) {
+		console.log(chalk.red(`Product \`${productId}\` not found on tenant \`${sourceAlias}\`.`))
+		exit(1)
+	}
+
+	return product
+}
+
+async function createTargetProduct(product, productId, targetContext, targetAlias) {
+	console.log(chalk.blue(`${chalk.bold(`2/8:`)} Creating product \`${productId}\` on tenant \`${targetAlias}\`.`))
+	return await targetContext.createProduct(product)
+}
+
+async function fetchSourceProductTree(product, sourceContext, sourceAlias) {
+	console.log(chalk.blue(`${chalk.bold(`3/8:`)} Fetch product tree \`${product.productTreeId}\` from tenant \`${sourceAlias}\`.`))
+	return await sourceContext.fetchProductTreeNodes(product.productTreeId)
+}
+
+async function createTargetProductTree(productTree, productId, targetContext, targetAlias) {
+	const totalQueries = productTree.length + productTree.reduce((acc, cur) => acc + cur?.fields?.length ?? 0, 0)
+	const idMap = {}
+	let rootNode = null
+
+	console.log(chalk.blue(`${chalk.bold(`4/8:`)} Copying ${totalQueries} node(s) from product \`${productId}\` to tenant \`${targetAlias}\`.`))
+
+	try {
+		if (productTree.length) {
+			console.log('')
+			const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
+			bar.start(totalQueries, 0)
+
+			let completed = 0
+
+			// make sure we execute these one at a time, do not spam the server with requests
+			for (const node of productTree) {
+				const id = generateId()
+				if (!rootNode) {
+					rootNode = id
+				}
+
+				idMap[node.id] = id
+
+				await targetContext.createNode(
+					id,
+					node.ref,
+					node.type,
+					node.alias,
+					node?.parent?.id ? {parentId: idMap[node.parent.id]} : null,
+					[
+						{
+							type: "String",
+							ref: "meta",
+							alias: "Meta",
+							resolver: {
+								text: "",
+								language: "CONSTANT"
+							}
+						}
+					]
+				)
+
+				completed++
+
+				bar.update(completed)
+				const fields = node.fields ? node.fields : []
+				for (const field of fields) {
+					await targetContext.attachFieldResolver(idMap[node.id], field.ref, field.resolver.text, field.resolver.language)
+					completed++
+					bar.update(completed)
+				}
+			}
+
+			bar.stop()
+		}
+	} catch (e) {
+		console.log('')
+		console.log('')
+		console.log(chalk.red(e.message))
+
+		if (e instanceof QueryError) {
+			console.log('')
+			console.log(chalk.blue(e.query))
+			console.log('')
+			console.log(chalk.blue(JSON.stringify(e.variables)))
+		}
+
+		exit(1)
+	}
+
+	console.log('')
+	console.log(chalk.green(`${chalk.bold('New root node:')} ${rootNode}`))
+
+	return rootNode
+}
+
+async function updateProductTreeId(product, productTreeId, targetContext, targetAlias) {
+	const productId =`${product.productId.plan}/${product.productId.type}/${product.productId.version}`
+	console.log(chalk.blue(`${chalk.bold(`5/8:`)} Updating \`${productId}\` with productTreeId \`${productTreeId}\` on \`${targetAlias}\`.`))
+	return await targetContext.updateProductTreeId(product.productId, productTreeId)
+}
+
+async function fetchProductSchema(sourceContext, productTreeId) {
+	console.log(chalk.blue(`${chalk.bold(`6/8:`)} Fetching data schemas for \`${productTreeId}\`).`))
+	return await sourceContext.fetchProductSchema(productTreeId)
+}
+
+async function createProductDataSchemas(targetContext, productTreeId, dataSchema) {
+	console.log(chalk.blue(`${chalk.bold(`7/8:`)} Create product schema for tree \`${productTreeId}\`.`))
+	return await targetContext.createProductSchema(productTreeId, dataSchema)
+}
+
+async function createProductUiDataSchemas(targetContext, productSchemaId, productTreeId, schema) {
+	console.log(chalk.blue(`${chalk.bold(`8/8:`)} Create product UI schema for tree \`${productTreeId}\`.`))
+	return await targetContext.createUiProductSchema(productSchemaId, productTreeId, schema)
+}
 
 program.command('product-nodes', 'Copy an entire product with data schemas, tree and nodes')
 	.argument('<tenant source alias>', 'Alias of a tenant to copy node types from')
@@ -24,75 +149,29 @@ program.command('product-nodes', 'Copy an entire product with data schemas, tree
 			const sourceContext = useGraphProduct(sourceTenant.environment.endpoint, sourceToken)
 			const targetContext = useGraphProduct(targetTenant.environment.endpoint, targetToken)
 
-			const [plan, name, version] = productId.split('/')
+			const product = await fetchSourceProduct(productId, sourceContext, sourceAlias)
+			const productCopy = await createTargetProduct(product, productId, targetContext, targetAlias)
+			const productTree = await fetchSourceProductTree(product, sourceContext, sourceAlias)
+			const rootNode = await createTargetProductTree(productTree, productId, targetContext, targetAlias)
+			await updateProductTreeId(product, rootNode, targetContext, targetAlias)
+			const schema = await fetchProductSchema(sourceContext, product.productTreeId)
+			const schemaId = await createProductDataSchemas(targetContext, rootNode, schema.dataSchema)
 
-			const product = await sourceContext.fetchProduct(plan, name, version)
-			if (!product) {
-				console.log(chalk.red(`Product \`${productId}\` not found on tenant \`${sourceAlias}\`.`))
-				exit(1)
-			}
-
-			console.log(chalk.green(`Creating product \`${productId}\` on tenant \`${targetAlias}\`.`))
-			// const productCopy = await targetContext.createProduct(product)
-			const productTree = await sourceContext.fetchProductTreeNodes(product.productTreeId)
-
-			console.log(chalk.green(`Copying ${productTree.length} nodes from product \`${productId}\` to tenant \`${targetAlias}\`.`))
-			if (productTree.length) {
-				const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
-				bar.start(productTree.length, 0)
-				let completed = 0
-
-				// make sure we execute these one at a time, do not spam the server with requests
-				for (const node of productTree) {
-					await targetContext.createNode(
-						node.id,
-						node.ref,
-						node.alias,
-						node.parent ? { parentId: node } : null,
-						[
-							{
-								type: "String",
-								ref: "meta",
-								alias: "Meta",
-								resolver: {
-									text: "",
-									language: "CONSTANT"
-								}
-							}
-						]
-					)
-
-					completed++
-					bar.update(completed)
-
-					const fields = node.fields ? node.fields : []
-					for (const field of fields) {
-						await targetContext.attachFieldResolver(node.id, field.ref, field.resolver.text, field.resolver.language)
-						completed++
-						bar.update(completed)
-					}
+			const uiSchemas = schema?.uiSchemas ?? []
+			for (const uiSchema of uiSchemas) {
+				if (uiSchema?.name === productCopy.productTreeId) {
+					await createProductUiDataSchemas(targetContext, schemaId, rootNode, uiSchema.schema)
 				}
-				bar.stop()
 			}
-			console.log(chalk.green(`Done copying ${productTree.length} nodes from product \`${productId}\` to tenant \`${targetAlias}\`.`))
+
+			console.log('')
+			console.log(chalk.bold.green(`Done!`))
 
 			exit(0)
 		} catch (e) {
 			console.error(chalk.red.bold(e.message))
 			exit(1)
 		}
-
-		// const { getConfig } = await import("./tenant/config.mjs")
-		// const { exportProductBuilderTree } = await import("./graph/export.mjs");
-		// const { importNodes } = await import("./graph/import.mjs");
-
-		// const sourceConfig = await getConfig(sourceName)
-		// const targetConfig = await getConfig(targetName)
-		// const nodes = await exportProductBuilderTree(nodeId, sourceConfig.TOKEN, sourceConfig.ENDPOINT)
-		// const rootNode = await importNodes(nodes, targetConfig.TOKEN, targetConfig.ENDPOINT)
-
-		// console.log("Import complete!")
-		// console.log(`New imported root node: ${rootNode}`)
 	})
 
 program.parse()
--- src/graph/product.mjs
+++ src/graph/product.mjs
@@ -3,7 +3,19 @@ import { print } from 'graphql'
 import axios from 'axios'
 import cliProgress from 'cli-progress'
 
+export class QueryError extends Error {
+	query = ''
+	variables = {}
+
+	constructor(message, query, variables) {
+		super(message)
+		this.query = query
+		this.variables = variables
+	}
+}
+
 export function useGraphProduct(endpoint, token) {
+
 	async function createProduct(product) {
 		const query = gql`
 			mutation cloneProduct(
@@ -27,29 +39,29 @@ export function useGraphProduct(endpoint, token) {
 			}
 		`
 
-		const response = await axios.post(endpoint, {
-			query: print(query),
-			variables: {
-				...product,
+		const response = await axios.post(
+			endpoint,
+			{
+				query: print(query),
+				variables: {
+					...product,
+				}
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${token}`,
+				}
 			}
-		}, {
-			headers: {
-				Authorization: `Bearer ${token}`,
-			}
-		})
+		)
 
 		if (response.data.errors) {
-			throw new Error(response.data.errors?.[0].message)
+			throw new QueryError(response.data.errors?.[0].message.trim(), print(query), product)
 		}
 
 		return response.data.data.createProduct
 	}
 
-	// async function defineType() {
-	// 	const quye
-	// }
-
-	async function createNode(id, ref, alias, position, fields) {
+	async function createNode(id, ref, type, alias, position, fields) {
 		const query = gql`
 			mutation importNode(
 				$id: ID!,
@@ -77,10 +89,10 @@ export function useGraphProduct(endpoint, token) {
 				variables: {
 					id,
 					ref,
+					type,
 					alias,
 					position,
-					fields,
-					type: 'string'
+					fields
 				}
 			},
 			{
@@ -91,7 +103,7 @@ export function useGraphProduct(endpoint, token) {
 		)
 
 		if (response.data.errors) {
-			throw new Error(response.data.errors?.[0].message)
+			throw new QueryError(response.data.errors?.[0].message)
 		}
 
 		return response.data.data.createNode
@@ -110,16 +122,18 @@ export function useGraphProduct(endpoint, token) {
 			}
 		`
 
+		const variables = {
+			nodeId,
+			fieldName,
+			text,
+			language
+		}
+
 		const response = await axios.post(
 			endpoint,
 			{
 				query: print(query),
-				variables: {
-					nodeId,
-					fieldName,
-					text,
-					language
-				}
+				variables,
 			},
 			{
 				headers: {
@@ -129,10 +143,45 @@ export function useGraphProduct(endpoint, token) {
 		)
 
 		if (response.data.errors) {
-			throw new Error(response.data.errors?.[0].message)
+			throw new QueryError(response.data.errors?.[0].message.trim(), print(query), variables)
 		}
 
 		return response.data.data.attachFieldResolver
+	}
+
+	async function updateProductTreeId(productId, productTreeId) {
+		const query = gql`
+			mutation updateProductTree($productId: productIdInput!, $productTreeId: String!) {
+				updateProduct(productId: $productId, input: { productTreeId: $productTreeId }) {
+					productTreeId
+				}
+			}
+		`
+
+		const variables = {
+			productId,
+			productTreeId
+		}
+
+		const response = await axios.post(
+			endpoint,
+			{
+				query: print(query),
+				variables,
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${token}`,
+				}
+			}
+		)
+
+		const errorMessage = response.data?.errors?.[0]?.message
+		if (errorMessage) {
+			throw new QueryError(errorMessage.trim(), print(query), variables)
+		}
+
+		return response.data.data?.updateProduct?.productTreeId
 	}
 
 	async function fetchProductTreeNodes(productTreeId) {
@@ -159,27 +208,29 @@ export function useGraphProduct(endpoint, token) {
 			}
 		`
 
-		try {
-			const response = await axios.post(
-				endpoint,
-				{
-					query: print(query),
-					variables: {
-						parentNodeId: productTreeId
-					}
-				},
-				{
-					headers: {
-						Authorization: `Bearer ${token}`,
-					}
-				}
-			)
+		const variables = {
+			parentNodeId: productTreeId
+		}
 
-			return response.data.data.listNodes
+		const response = await axios.post(
+			endpoint,
+			{
+				query: print(query),
+				variables
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${token}`,
+				}
+			}
+		)
+
+		const errorMessage = response.data?.errors?.[0]?.message
+		if (errorMessage) {
+			throw new QueryError(errorMessage.trim(), print(query), variables)
 		}
-		catch (e) {
-			console.error(e)
-		}
+
+		return response.data.data.listNodes
 	}
 
 	async function fetchProduct(plan, type, version) {
@@ -230,11 +281,125 @@ export function useGraphProduct(endpoint, token) {
 		}
 	}
 
+	async function fetchProductSchema(productTreeId) {
+		const query = gql`
+			query productSchema($nodeId: ID!) {
+				productSchema(nodeId: $nodeId) {
+					id
+					nodeId
+					dataSchema
+					uiSchemas {
+						name
+						schema
+					}
+				}
+			}
+		`
+
+		const variables = {
+			nodeId: productTreeId
+		}
+
+		const response = await axios.post(
+			endpoint,
+			{
+				query: print(query),
+				variables
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
+			}
+		)
+
+		const errorMessage = response.data?.errors?.[0]?.message
+		if (errorMessage) {
+			throw new QueryError(errorMessage.trim(), print(query), variables)
+		}
+
+		return response.data.data.productSchema
+	}
+
+	async function request(endpoint, query, variables) {
+		const response = await axios.post(
+			endpoint,
+			{
+				query: print(query),
+				variables
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
+			}
+		)
+
+		const errorMessage = response.data?.errors?.[0]?.message
+		if (errorMessage) {
+			throw new QueryError(errorMessage.trim(), print(query), variables)
+		}
+
+		return response.data.data
+	}
+
+	async function createProductSchema(productTreeId, dataSchema) {
+		const query = gql`
+			mutation updateSchema($nodeId: ID!, $dataSchema: String!) {
+				createProductSchema(input: {
+					nodeId: $nodeId,
+					dataSchema: $dataSchema
+				}) {
+					value
+					status
+				}
+			}
+		`
+
+		const variables = {
+			nodeId: productTreeId,
+			dataSchema
+		}
+
+		const response = await request(endpoint, query, variables)
+		return response?.createProductSchema?.value
+	}
+
+	async function createUiProductSchema(productSchemaId, productTreeId, schema) {
+		const query = gql`
+			mutation updateSchema($productSchemaId: ID!, $name: String!, $schema: String!) {
+				addUiSchemaToProductSchema(
+					productSchemaId: $productSchemaId
+					input: {
+						name: $name,
+						schema: $schema
+					}
+				) {
+					status
+					errors
+				}
+			}
+		`
+
+		const variables = {
+			productSchemaId: productTreeId,
+			name: productTreeId,
+			schema: schema,
+		}
+
+		const response = await request(endpoint, query, variables)
+		return response?.addUiSchemaToProductSchema?.status
+	}
+
 	return {
 		createProduct,
+		updateProductTreeId,
 		fetchProductTreeNodes,
 		fetchProduct,
 		createNode,
 		attachFieldResolver,
+		createProductSchema,
+		createUiProductSchema,
+		fetchProductSchema,
 	}
 }
