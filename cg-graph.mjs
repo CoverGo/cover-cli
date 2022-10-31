05bf69bf2e9e3044b88342abe783f635220bd199
Richard Vanbergen <rich.vanbergen@gmail.com>
2022-10-31 14:56:00 +0900

    removing experimental commands

--- cg-graph.mjs
+++ cg-graph.mjs
@@ -11,6 +11,5 @@ program.command('product-node-type', 'Manage product node types')
 program.command('product-tree', 'Manage product trees')
 program.command('product-schema', 'Manage product schemas')
 program.command('file', 'Manage external tables')
-program.command('tenant', 'Manage tenants on the API')
 
 program.parse()
