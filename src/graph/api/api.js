14df208b61d13679b4df4d83ff5d52c42064d9bb
Richard Vanbergen <rich.vanbergen@gmail.com>
2022-11-01 15:29:08 +0900

    move error handling to consumer methods
    had to make too many assumptions about shape of data and how it's handled

--- src/graph/api/api.js
+++ src/graph/api/api.js
@@ -1,32 +1,5 @@
 import axios from 'axios'
 import { print } from 'graphql'
-import { chalk } from 'zx'
-import { exit } from 'node:process'
-
-export class QueryError extends Error {
-	query = ''
-	variables = {}
-
-	constructor(message, query, variables) {
-		super(message)
-		this.query = query
-		this.variables = variables
-	}
-}
-
-export function handleExceptionInQuery(e) {
-	console.log('')
-	console.log('')
-	console.log(chalk.red(e.message))
-
-	if (e instanceof QueryError) {
-		console.log('')
-		console.log(chalk.blue(e.query))
-		console.log(chalk.blue(JSON.stringify(e.variables)))
-	}
-
-	exit(1)
-}
 
 export function getGraphEndpoint(endpoint) {
 	return `${endpoint}/graphql`
@@ -62,7 +35,7 @@ export function createRequest(environment, token) {
 	return async function request(query, variables = undefined) {
 		const payload = variables ? { query: print(query), variables } : { query: print(query) }
 
-		const response = await axios.post(
+		return await axios.post(
 			getGraphEndpoint(environment.endpoint),
 			payload,
 			{
@@ -71,12 +44,5 @@ export function createRequest(environment, token) {
 				}
 			}
 		)
-
-		const errorMessage = response.data?.errors?.[0]?.message
-		if (errorMessage) {
-			throw new QueryError(errorMessage.trim(), print(query), variables)
-		}
-
-		return response.data.data
 	}
 }
