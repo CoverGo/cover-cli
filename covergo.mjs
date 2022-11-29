81a2a785340037aa5e974703e1b342d524f138cb
Richard Vanbergen <rich.vanbergen@gmail.com>
2022-11-29 15:49:26 +0900

    update package version

--- covergo.mjs
+++ covergo.mjs
@@ -6,7 +6,7 @@ const program = new Command();
 program
 	.name('covergo')
 	.description('Utility scripts for interacting with the covergo platform')
-	.version('2.2.3')
+	.version('2.2.4')
 	.command('graph', 'Interact with the graph API')
 	.command('env', 'Manage environments')
 	.command('tenant', 'Manage tenants')
