b24abe7d418f67b4454cac4f1df90f49ecb731d6
Richard Vanbergen <rich.vanbergen@gmail.com>
2023-02-14 10:55:08 +0900

    poitential bugfix for creating config

--- covergo.mjs
+++ covergo.mjs
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
+import { Command } from "commander";
 const program = new Command();
 
 program
-	.name('covergo')
-	.description('Utility scripts for interacting with the covergo platform')
-	.version('2.2.5')
-	.command('graph', 'Interact with the graph API')
-	.command('env', 'Manage environments')
-	.command('tenant', 'Manage tenants')
+  .name("covergo")
+  .description("Utility scripts for interacting with the covergo platform")
+  .version("2.2.6")
+  .command("graph", "Interact with the graph API")
+  .command("env", "Manage environments")
+  .command("tenant", "Manage tenants");
 
-program.parse()
+program.parse();
