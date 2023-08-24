5ecf98201ea6b8dcb4040ce145ce0f94b0dc2d84
Richard Vanbergen <rich.vanbergen@gmail.com>
2023-08-24 11:33:04 +0900

    fix accidentally deleted main file

---
+++ covergo.mjs
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import { Command } from "commander"
+const program = new Command()
+
+program
+	.name("covergo")
+	.description("Utility scripts for interacting with the covergo platform")
+	.version("2.2.18")
+	.command("graph", "Interact with the graph API")
+	.command("env", "Manage environments")
+	.command("tenant", "Manage tenants")
+
+program.parse()
